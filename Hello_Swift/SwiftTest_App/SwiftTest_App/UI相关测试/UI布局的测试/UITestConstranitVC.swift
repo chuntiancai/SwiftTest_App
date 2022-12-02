9a4433f7bca10a3d898a4dd008ae48065cf81e2a
chuntiancai <626802575@qq.com>
2022-12-02 17:28:24 +0800

    逆向工程

--- Hello_Swift/SwiftTest_App/SwiftTest_App/UI相关测试/UI布局的测试/UITestConstranitVC.swift
+++ Hello_Swift/SwiftTest_App/SwiftTest_App/UI相关测试/UI布局的测试/UITestConstranitVC.swift
@@ -28,7 +28,7 @@
         也就是viewB再设置参考viewC的约束，但是优先级要比参考viewA的约束低，这样viewA被删除后，viewB还是可以参考viewC的约束。
  
     6、实现约束的动画效果，必须是在UIView的动画方法里立即更新父view的布局约束，更新自身的没用。因为在使用约束添加动画的时候，有个原则就是动画要添加到当前视图的父视图上。
-       因为约束最终是反映在frame上，所以要在UIView的动画block里面调用父view的layoutIfNeeded()方法，这是不等下一个周期里面更新UI，也就是里面设置frame， 这和直接设置frame的效果是一样的，如果是调用setNeedsLayout()方法，则当前修改的约束还没应用到frame上，也就是frame的值还是之前的，要等到下一个UI周期才会应用到frame上， 所以在UIview的动画block里的frame值还是旧的值，所以就不会用动画效果，而是在下一个UI周期时，直接就修改了view的frame就直接渲染了，没有在动画效果。
+       因为约束最终是反映在frame上，所以要在UIView的动画block里面调用父view的layoutIfNeeded()方法，这是不等下一个周期里面更新UI，也就是里面设置frame， 这和直接设置frame的效果是一样的，如果是调用setNeedsLayout()方法，则当前修改的约束还没应用到frame上， 也就是frame的值还是之前的， 要等到下一个UI周期才会应用到frame上， 所以在UIview的动画block里的frame值还是旧的值，所以就不会用动画效果，而是在下一个UI周期时，直接就修改了view的frame就直接渲染了，没有在动画效果。
  
     7、translatesAutoresizingMaskIntoConstraints 的本意是将 frame 布局 自动转化为 约束布局，转化的结果是为这个视图自动添加所有需要的约束， 如果我们这时给视图添加自己创建的约束就一定会约束冲突。为了避免上面说的约束冲突，我们在代码创建 约束布局 的控件时 直接指定这个视图不能用frame 布局 （即translatesAutoresizingMaskIntoConstraints=NO），可以放心的去使用约束了。
 
