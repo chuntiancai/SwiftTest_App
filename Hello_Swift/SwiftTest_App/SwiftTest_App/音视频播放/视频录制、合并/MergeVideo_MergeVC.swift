a6d4d95aa3dda6c1f648ffea1a139c62fb5e0340
chuntiancai <626802575@qq.com>
2023-03-02 17:52:31 +0800

    视频编辑

---
+++ Hello_Swift/SwiftTest_App/SwiftTest_App/音视频播放/视频录制、合并/MergeVideo_MergeVC.swift
@@ -0,0 +1,9 @@
+//
+//  MergeVideo_MergeVC.swift
+//  SwiftTest_App
+//
+//  Created by mathew on 2023/2/23.
+//  Copyright © 2023 com.mathew. All rights reserved.
+//
+
+import Foundation
---
+++ Hello_Swift/SwiftTest_App/SwiftTest_App/音视频播放/视频录制、合并/TestVideoMerge_VC.swift
@@ -0,0 +1,9 @@
+//
+//  TestVideoMerge_VC.swift
+//  SwiftTest_App
+//
+//  Created by mathew on 2023/2/21.
+//  Copyright © 2023 com.mathew. All rights reserved.
+//
+
+import Foundation
