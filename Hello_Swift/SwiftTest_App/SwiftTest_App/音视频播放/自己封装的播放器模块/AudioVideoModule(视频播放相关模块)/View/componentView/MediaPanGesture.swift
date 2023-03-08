54a4abb0cf4a82e298e8d6d6e902a931f6243ca8
chuntiancai <626802575@qq.com>
2023-03-08 17:55:06 +0800

    要测试屏幕旋转和视频的关系

---
+++ Hello_Swift/SwiftTest_App/SwiftTest_App/音视频播放/自己封装的播放器模块/AudioVideoModule(视频播放相关模块)/View/componentView/MediaPanGesture.swift
@@ -0,0 +1,13 @@
+//
+//  MediaPanGesture.swift
+//  SwiftTest_App
+//
+//  Created by mathew on 2023/3/8.
+//  Copyright © 2023 com.mathew. All rights reserved.
+//
+
+import UIKit
+
+class MediaPanGesture: UIPanGestureRecognizer {
+
+}
